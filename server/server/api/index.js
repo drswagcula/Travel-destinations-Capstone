eb27b4018d654e7098580f1bb78fa42602e80730
dayrich96 <daynalisa7596@gmail.com>
2025-06-03 21:01:21 -0500

    init

--- server/server/api/index.js
+++ server/server/api/index.js
@@ -2,6 +2,12 @@ const router = require('express').Router();
 const prisma = require('../utils/prismaClient');
 const { authenticateToken } = require('../middleware/auth');
 
+const authRoutes = require('../routes/auth')
+const destinationRoutes =  require('../routes/destinations')
+const reviewRoutes = require('../routes/reviews')
+const reportRoutes = require('../routes/reports')
+const userRoutes = require('../routes/users')
+
 
 router.use(async (req, res, next) => {
     try {
@@ -51,24 +57,24 @@ router.use(async (req, res, next) => {
 });
 
 
-const apiRoutes = [
-    { path: '/auth', route: require('../routes/auth') },
-    { path: '/destinations', route: require('../routes/destinations') },
-    { path: '/reviews', route: require('../routes/reviews') },
-    { path: '/reports', route: require('../routes/reports') },
-    { path: userRoutes = require('../routes/users')}
-];
+//const apiRoutes = [
+//    { path: '/auth', route: require('../routes/auth') },
+//    { path: '/destinations', route: require('../routes/destinations') },
+//    { path: '/reviews', route: require('../routes/reviews') },
+//    { path: '/reports', route: require('../routes/reports') },
+//    { path: userRoutes = require('../routes/users')}
+//];
 
 
-outer.use('/auth', authRoutes); 
+router.use('/auth', authRoutes); 
 router.use('/users', userRoutes);
-router.use('/destinations', destinationRoutes); s
+router.use('/destinations', destinationRoutes); 
 router.use('/reviews', reviewRoutes); 
 router.use('/reports', reportRoutes);
 
-apiRoutes.forEach(({ path, route }) => {
-    router.use(path, route);
-});
+//apiRoutes.forEach(({ path, route }) => {
+ //   router.use(path, route);
+//});
 
 
 router.use((req, res, next) => {
